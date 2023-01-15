fffc0a2d58edc2b8b861cfa196d632bb039cd55d
suwonleee <suwonleee@naver.com>
2023-01-15 15:42:42 +0900

    Recoil apply files

--- 1.moneyMind_React/moneyMind/src/App.js
+++ 1.moneyMind_React/moneyMind/src/App.js
@@ -1,32 +1,40 @@
 import React, {useState} from "react";
-// import ReactDOM from "react-dom/client";
+import { atom, useRecoilState } from "recoil";
+
+const page1NoAtom = atom({
+  key: "app/page1NoAtom",
+  default: 0
+});
+
+const page2NoAtom = atom({
+  key: "app/page2NoAtom",
+  default: 0
+});
 
 function Page1() {
-  const [no, setNo] = useState(0);
+  const [no, setNo] = useRecoilState(page1NoAtom);
 
   return (
     <>
       <h1>페이지 1</h1>
-      
       <ul>
         <li>페이지 1의 숫자 : {no}</li>
         <li><button onClick={() => setNo(no + 10)}>+10</button></li>
         <li><button onClick={() => setNo(no - 10)}>-10</button></li>
-        <li>페이지 1 내용 2</li>
       </ul>
     </>
   )
 }
 
 function Page2() {
-  const [no, setNo] = useState(0);
+  const [no, setNo] = useRecoilState(page2NoAtom);
 
   return (
     <>
       <h1>페이지 2</h1>
       
       <ul>
-        <li>페이지 2의 숫자 : {no}</li>
+        <li>운동 횟수 : {no}</li>
         <li><button onClick={() => setNo(no + 10)}>+10</button></li>
         <li><button onClick={() => setNo(no - 10)}>-10</button></li>
       </ul>
@@ -36,15 +44,15 @@ function Page2() {
 
 function App() {
   const [pageName, setPageName] = useState('page1');
-  
-  const switchPage = () => setPageName(pageName == 'page1' ? 'page2' : 'page1');
-  return <>
-    <button onClick={switchPage}>스위치</button>
-    {pageName == 'page1' && <Page1/>}
-    {pageName == 'page2' && <Page2/>}
-    <div className="App">하하 안녕하세요.</div>
-
-  </>
+  const switchPage = () => setPageName(pageName === 'page1' ? 'page2' : 'page1');
+  return (
+    <>
+      <button onClick={switchPage}>스위치</button>
+      {pageName === 'page1' && <Page1/>}
+      {pageName === 'page2' && <Page2/>}
+      <div className="App">하하 안녕하세요.</div>
+    </>
+  );
 }
 
 export default App;
--- 1.moneyMind_React/moneyMind/src/index.js
+++ 1.moneyMind_React/moneyMind/src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RecoilRoot } from "recoil";
 import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <RecoilRoot>
     <App />
-  </React.StrictMode>
+  </RecoilRoot>
 );
