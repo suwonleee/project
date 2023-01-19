cca19baa9bf83e90bd26f853fc9306ac5a802bdb
suwonleee <suwonleee@naver.com>
2023-01-19 21:43:06 +0900

    Add NoticeSnackbar.js comment part.

--- 1.moneyMind_React/moneyMind/src/components/NoticeSnackbar.js
+++ 1.moneyMind_React/moneyMind/src/components/NoticeSnackbar.js
@@ -1,3 +1,5 @@
+//  EditTodoModal에도, NewTodoForm 에도, BottomOptionDrawer에도,
+//  NoticeSnackbar를 사용한다.
 import { Snackbar, Alert as MuiAlert } from "@mui/material";
 import React from "react";
 import { atom, useRecoilState } from "recoil";
@@ -17,11 +19,13 @@ export function useNoticeSnackbarStatus() {
     noticeSnackbarInfoAtom
   );
 
-  const opened = noticeSnackbarInfo.opened;
-  const autoHideDuration = noticeSnackbarInfo.autoHideDuration;
-  const severity = noticeSnackbarInfo.severity;
-  const msg = noticeSnackbarInfo.msg;
+  const opened = noticeSnackbarInfo.opened; //열린지 유무 확인.
+  const autoHideDuration = noticeSnackbarInfo.autoHideDuration; //얼마나 후에 자동으로 닫히는지.
+  const severity = noticeSnackbarInfo.severity; //심각성(배경 색상)
+  const msg = noticeSnackbarInfo.msg; //메세지
 
+  //autoHideDuration : 스낵바가 6초 정도 유지된다. 
+  // open 상황을 예시로 함수 만들기. (현재 열려있는지 아닌지.
   const open = (msg, severity = "success", autoHideDuration = 6000) => {
     setNoticeSnackbarInfo({
       opened: true,
@@ -62,6 +66,7 @@ export function NoticeSnackbar() {
         autoHideDuration={status.autoHideDuration}
         onClose={status.close}
       >
+      {/* severity={status.severity}는 warining/ success으로 하면 배경 색상이 바뀌는 부분 */}
         <Alert severity={status.severity}>{status.msg}</Alert>
       </Snackbar>
     </>
