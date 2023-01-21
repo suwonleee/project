955b389481cd739765b8e89912cc2661b5708de7
suwonleee <suwonleee@naver.com>
2023-01-21 14:40:51 +0900

    Countup library comment part upload

--- 1.moneyMind_React/moneyMind/src/components/CountUp.js
+++ 1.moneyMind_React/moneyMind/src/components/CountUp.js
@@ -1,19 +1,26 @@
 
 import { CountUp } from "countup.js";
 import { useEffect, useRef } from "react";
+//CountUp 라이브러리 활용하기
+//활용 확인 : https://codepen.io/suwonleee/pen/gOjozwQ?editors=0010
+
 
 export function CountNumber({ start = 0, end = 1000, duration = 2 }) {
+  //useRef : DOM 조작 / 기억했다가 리로드 시 같이 리로드
   const spanRef = useRef(null);
   const countUpRef = useRef(null);
 
+  //useEffect : 딱 한번만 실행되어야하는 경우 사용
   useEffect(() => {
     if (countUpRef.current == null) {
       countUpRef.current = new CountUp(spanRef.current, end, {
+        //시작 숫자 설정
         startVal: start,
+        //얼마동안 그 숫자를 유지할건지
         duration: duration,
         formattingFn: (num) => String(num).padStart(5, "0"),
       });
-      countUpRef.current.start();
+      countUpRef
     } else {
       countUpRef.current.update(end);
     }
