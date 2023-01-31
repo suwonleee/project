4a41b5d232a04bdf890a46c82003fea12b46acf5
suwonleee <suwonleee@naver.com>
2023-01-31 23:55:45 +0900

    Make CRUD example file using useState and supplementd the comments in the basic file.

--- 1.moneyMind_React/moneyMind/src/0.basic_reactUsage/9.react_CRUD/1.indexPlus1_useState.js
+++ 1.moneyMind_React/moneyMind/src/0.basic_reactUsage/9.react_CRUD/1.indexPlus1_useState.js
@@ -1,28 +1,32 @@
-//https://codepen.io/suwonleee/pen/LYBgRXK?editors=0010
+//https://codepen.io/suwonleee/pen/rNrqWMR?editors=0010
 
 import React, { useState } from "https://cdn.skypack.dev/react";
 import ReactDOM from "https://cdn.skypack.dev/react-dom";
 
 function App() {
-  //버튼을 클릭할 때마다 담을 리스트를 useState에 담기.
   const [todos, setTodos] = useState([]);
+  //useState에 인덱스로 받아서 넘길 값을 지정
+  const [lastTodoId, setLastTodoId] = useState(0);
 
-  //! addTodo 지정
+  //! addTodo 시작 부분
   const addTodo = (newContent) => {
 
-    // Todo 만약 id가 추가될 때마다 하나씩 늘어나고 싶다면 ???? -> 다음 파일
+    //id를 현재 나타낼 값 지정
+    const id = lastTodoId + 1;
+    //useState로 다음 값 넘기기.
+    setLastTodoId(id);
+    
     const newTodo = {
-      id: 1,
+      id,
       content: newContent,
       regDate: "2023-01-31 11:47:30"
     };
 
-    //새로운 변수에 값을 추가 -> useState로 값 넘기기
+    //새로운 변수에 값을 추가 -> useState로 다음 값 넘기기
     const newTodos = [...todos, newTodo];
     setTodos(newTodos);
   };
 
-  //! 버튼을 클릭했을때 추가할 문구 지정
   const onBtnAddTodoClick = () => {
     addTodo("안녕");
   };
