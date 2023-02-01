33f1b7dfa31dbaf52511d6e74edaca7acd0d27fc
suwonleee <suwonleee@naver.com>
2023-02-01 17:31:50 +0900

    Completely delete Crud files uploaded

--- 1.moneyMind_React/moneyMind/src/0.basic_reactUsage/9.react_CRUD/3.delete_CRUD.js
+++ 1.moneyMind_React/moneyMind/src/0.basic_reactUsage/9.react_CRUD/3.delete_CRUD.js
@@ -26,7 +26,7 @@ console.log(newTodo);
 
 const index = 2;
 const newContent = '새로 다른 콘텐츠로 바꿔준다.';
-// ! 어떤 값을 삭제하거나 수정할 땐, map을 사용해준다.
+// ! 어떤 값을 수정할 땐, map을 사용해준다.
 // _index 차례가 index(2)와 같지 않으면 todo를 그대로 담아주고,
 // 해당 인덱스가 되면 todo를 그대로 받아주고, content만 newContent로 바꿔준다.
 const newTodos = todos.map((todo, _index) => _index != index ? todo : {...todo, list:newContent})
---
+++ 1.moneyMind_React/moneyMind/src/0.basic_reactUsage/9.react_CRUD/4.deleteNo2_CRUD.js
@@ -0,0 +1,56 @@
+//https://codepen.io/suwonleee/pen/XWByJXa?editors=0010
+
+import React, { useState, useRef } from "https://cdn.skypack.dev/react";
+import ReactDOM from "https://cdn.skypack.dev/react-dom";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const lastTodoIdRef = useRef(0);
+
+  const addTodo = (newContent) => {
+    //추가를 할 때마다 현재 + 1 되어 값이 나오도록
+    const id = ++lastTodoIdRef.current;
+    
+    const newTodo = {
+      id,
+      content: newContent,
+      regDate: "2022-04-19 12:12:12"
+    };
+
+    const newTodos = [...todos, newTodo];
+    setTodos(newTodos);
+  };
+  
+  // ! 어떤 값을 삭제할 땐 filter를 이용해준다.
+  const removeTodo = (index) => {
+    // todo filter 활용법을 외워두기 !!!
+    const newTodos = todos.filter((_, _index) => _index != index);
+    setTodos(newTodos);
+  };
+
+  const onBtnAddTodoClick = () => {
+    addTodo("안녕");
+  };
+  
+  const onBtnDeleteTodoClick = () => {
+    //두번째 인덱스를 삭제
+    removeTodo(1);
+  };
+
+  return (
+    <>
+      <button onClick={onBtnAddTodoClick}>추가</button>
+      <button onClick={onBtnDeleteTodoClick}>삭제</button>
+      <hr />
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo.id} {todo.regDate} {todo.content}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
