e6019a6ef67e00a302363bcef41d998f4112dd86
suwonleee <suwonleee@naver.com>
2023-02-07 23:56:23 +0900

    Create mini project todo list and App bar file.

---
+++ 1.moneyMind_React/moneyMind/src/0.basic_reactUsage/13.miniproject_Mui/0.appBar.js
@@ -0,0 +1,54 @@
+//https://codepen.io/suwonleee/pen/oNMOmZE?editors=0010
+
+const { useState } = React;
+
+import classNames from "https://cdn.skypack.dev/classnames";
+
+const {
+  colors,
+  CssBaseline,
+  ThemeProvider,
+  createTheme,
+  Link,
+  Button,
+  AppBar,
+  Toolbar
+} = MaterialUI;
+
+function App() {
+  return (
+    <>
+      <AppBar position="fixed">
+        <Toolbar>
+          <div className="flex-1"></div>
+          <span className="font-bold">HAPPY NOTE</span>
+          <div className="flex-1"></div>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+    </>
+  );
+}
+
+function Root() {
+  // Create a theme instance.
+  const theme = createTheme({
+    typography: {
+      fontFamily: ["GmarketSansMedium"]
+    },
+    palette: {
+      primary: {
+        main: "#af8686",
+        contrastText: "#ffffff"
+      }
+    }
+  });
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  );
+}
+
+ReactDOM.render(<Root />, document.getElementById("root"));
