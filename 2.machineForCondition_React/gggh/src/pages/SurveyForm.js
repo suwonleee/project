6180bee61635f307c7a16d1e2519f72aa94bbd45
suwonleee <suwonleee@naver.com>
2023-02-23 20:25:29 +0900

    Qustion format upload.

--- 2.machineForCondition_React/gggh/src/pages/SurveyForm.js
+++ 2.machineForCondition_React/gggh/src/pages/SurveyForm.js
@@ -18,34 +18,40 @@ function SurveyForm (){
     <>
       <Grid container container
       spacing={2}
-      rowSpacing={2}
+      rowSpacing={10}
       columnSpacing={2}
       columns={16}
       sx={gridStyles}>
         <Grid item xs={12} sm={18} sx={{backgroundColor: "white"}}>
           <Box bgcolor="primary.main" color="info.contrastText" p={2}>
-            1
+            Q. 질문 파트 입니다
           </Box>
         </Grid>
         
         <Grid item xs={6} sm={3}>
           <Box bgcolor="error.main" color="info.contrastText" p={2}>
-            4
+            1 <br/>전혀 그렇지 않다.
+
           </Box>
         </Grid>
         <Grid item xs={6} sm={3}>
           <Box bgcolor="error.main" color="info.contrastText" p={2}>
-            5
+            2 <br/> 그렇지 않다.
           </Box>
         </Grid>
         <Grid item xs={6} sm={3}>
           <Box bgcolor="error.main" color="info.contrastText" p={2}>
-            6
+            3 <br/> 보통이다.
           </Box>
         </Grid>
         <Grid item xs={6} sm={3}>
           <Box bgcolor="error.main" color="info.contrastText" p={2}>
-            7
+            4 <br/> 그렇다.
+          </Box>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Box bgcolor="error.main" color="info.contrastText" p={2}>
+          5 <br/> 매우 그렇다.
           </Box>
         </Grid>
       </Grid>
